759c29da5db5dfa336ccfda533ab45bf106842fa
melikaeslahi <melika.eslahi@gmail.com>
2024-07-03 23:00:38 +0330

    update deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    basePath: "/quizapp",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
 };
